dabfc63eba6036bc77139d2b09796b2b2dc5ad93
Anderson David <Anderson@Andersons-MacBook-Air.local>
2018-10-26 16:13:36 -0400

    edited Procfile

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var url = require('url');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
   app.use(express.static(path.join(__dirname, '/styles')));
   app.use(express.static(path.join(__dirname, '/pages')));
